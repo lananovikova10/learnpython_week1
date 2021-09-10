e9eff79d522b09cfdc720d224cddf77adcb641f9
Svetlana Novikova <svetlana.novikova@alibaba-inc.com>
2021-09-10 12:25:33 +0300

    fix exceptions task - removed double input

--- exceptions_task1.py
+++ exceptions_task1.py
@@ -1,17 +1,19 @@
 # Перепишите функцию hello_user() из задания про while, чтобы она перехватывала KeyboardInterrupt, 
 # писала пользователю "Пока!" и завершала работу при помощи оператора break
 
-def hello_user(user_answer):
-    while True:
-        if user_answer == 'Хорошо':
-            print('Наконец-то!')
-            break
-        else:
-            try:
-                user_answer = input('Как дела? ')
-            except KeyboardInterrupt:
-                print('Пока!')
+
+
+def hello_user():
+    try:
+        while True:
+            user_answer = input('Как дела? ')
+            if user_answer != 'Хорошо':
+                pass
+            else: 
+                print('Наконец-то!')  
                 break
 
-user_answer = input('Как дела? ')
-hello_user(user_answer)
+    except KeyboardInterrupt:
+        print('Пока!')
+
+hello_user()
